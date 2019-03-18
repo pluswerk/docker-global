64bae4688fc5926ba03aa91207eab434c2459649
Cyb10101 <cyb10101@gmail.com>
2019-03-18 11:48:36 +0100

    [CLEANUP] No return on end needed

--- start.sh
+++ start.sh
@@ -6,38 +6,31 @@ function startFunction {
   case ${key} in
      upgrade)
         git fetch && git checkout master && git pull
-        return
         ;;
      start)
         startFunction pull
         startFunction build
         startFunction up
-        return
         ;;
      up)
         docker-compose up -d
-        return
         ;;
      stopOther)
         containers=$(docker ps --filter network=global -q)
         if [[ "$containers" ]]; then
           docker stop $(printf "%s" "${containers}")
         fi
-        return
         ;;
      down)
         startFunction stopOther
         docker-compose down --remove-orphans
-        return
         ;;
      stop)
         startFunction stopOther
         docker-compose stop --remove-orphans
-        return
         ;;
      *)
         docker-compose "${@:1}"
-        return
         ;;
   esac
 }
