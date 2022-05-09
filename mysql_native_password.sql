dfbadd9378168c2c331c643b277b3911faa8b0c6
Tomek Ziaja <t.ziaja@andersundsehr.com>
2022-05-09 14:51:20 +0200

    fix mysql8 user creation

--- mysql_native_password.sql
+++ mysql_native_password.sql
@@ -1,2 +1,6 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+FLUSH PRIVILEGES;
