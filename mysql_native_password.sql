80c0cdc5e622c9743fb4ff6a4b7ba6e8bbe412c9
Matthias Vogel <m.vogel@andersundsehr.com>
2024-04-11 12:47:42 +0200

    FIX user already exists error + grant all options mysql_native_password.sql

--- mysql_native_password.sql
+++ mysql_native_password.sql
@@ -1,6 +1,6 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON * . * TO 'root'@'%';
+GRANT ALL PRIVILEGES ON * . * TO 'root'@'%' WITH GRANT OPTION;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
 FLUSH PRIVILEGES;
